839c12d889d66075966dc9a083f15b8f8db72a10
Mateus Fernandes Mano <102445669+MattiasDoCss@users.noreply.github.com>
2023-09-11 19:46:20 -0300

    Quarto commit

--- listaDeExercícios01.sql
+++ listaDeExercícios01.sql
@@ -1,7 +1,15 @@
+--Exercício 1:
 SELECT titulo FROM livros;
 
+--Exercício 2:
 SELECT nome FROM autores WHERE nascimento < 1900-01-01;
 
+--Exercício 3:
 SELECT titulo, autor_id, nome FROM livros 
 JOIN autores a ON a.id = l.autor_id
 WHERE a.nome = 'J.K Rowling';
+
+--Exercício 4:
+SELECT aluno_id, nome, curso FROM alunos a
+LEFT JOIN matriculas m ON m.id  = a.id
+WHERE curso = "Engenharia de Software";
