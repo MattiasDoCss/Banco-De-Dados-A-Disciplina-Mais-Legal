e02efa46c474b656184c542aba0dd3e7812420cd
EA - Mateus Fernandes Mano <mateus_mano@educadventista.org>
2023-09-11 16:41:08 -0300

    terceiro commit

--- listaDeExercícios01.sql
+++ listaDeExercícios01.sql
@@ -1,3 +1,7 @@
 SELECT titulo FROM livros;
 
 SELECT nome FROM autores WHERE nascimento < 1900-01-01;
+
+SELECT titulo, autor_id, nome FROM livros 
+JOIN autores a ON a.id = l.autor_id
+WHERE a.nome = 'J.K Rowling';
