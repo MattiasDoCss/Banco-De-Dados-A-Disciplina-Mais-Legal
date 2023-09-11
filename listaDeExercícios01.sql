406ec4fa2a751588650a27feb5a59949256d28a4
Mateus Fernandes Mano <102445669+MattiasDoCss@users.noreply.github.com>
2023-09-11 19:52:23 -0300

    Sexto commit

--- listaDeExercícios01.sql
+++ listaDeExercícios01.sql
@@ -17,3 +17,8 @@ WHERE curso = "Engenharia de Software";
 --Exercício 5:
 SELECT produto, SUM(receita) AS total.r FROM vendas
 GROUP BY produto;
+
+--Exercício 6:
+SELECT nome, COUNT(autor_id) AS total_livros FROM livros  
+JOIN autores a ON a.id = l.autor_id
+GROUP BY nome;
